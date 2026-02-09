3daf3d993282df73c93151aa05296561d558f3af
Giorgian Robescu <robescu.george96@gmail.com>
2026-02-09 15:40:19 +0200

    Added basic index js

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
+});
+
+io.on('connection', (socket) => {
+    // User connected, notify others
+    socket.broadcast.emit('notification', 'User X has connected');
+
+    // Handle incoming message
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    // Handle user leaving
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('notification', 'User X has disconnected');
+    });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
