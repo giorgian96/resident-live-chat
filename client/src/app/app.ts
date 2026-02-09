d9ce17e641a5491c206e09b63ee712291eef4a7f
Giorgian Robescu <robescu.george96@gmail.com>
2026-02-09 16:16:11 +0200

    Added components to app

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatDisplay } from './chat-display/chat-display';
+import { ChatBox } from './chat-box/chat-box';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChatDisplay, ChatBox],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = signal('Resident Live Chat');
 }
--- client/src/app/chat-display/chat-display.ts
+++ client/src/app/chat-display/chat-display.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatMessage } from '../chat-message/chat-message';
+import { ChatNotification } from '../chat-notification/chat-notification';
 
 @Component({
   selector: 'app-chat-display',
-  imports: [],
+  imports: [ChatMessage, ChatNotification],
   templateUrl: './chat-display.html',
   styleUrl: './chat-display.css',
 })
